50643ff3bef3c7bf871a7418c2e1c96020514ef8
Justin <justin2004@hotmail.com>
2020-12-01 09:21:52 -0500

    day1

---
+++ day1/a.lisp
@@ -0,0 +1,24 @@
+(ql:quickload :april)
+(in-package :april)
+
+(setf *i* (with-open-file (s "input.txt" 
+                   :direction :input)
+  (loop 
+    :for line = (read-line s nil)
+    :while (not (null line))
+    :collect line)))
+
+; get the input into april
+(april:april (with (:state :in 
+                    ((i (coerce (mapcar #'parse-integer *i*) 
+                                'vector)))) )
+             "input←i")
+
+; part 1
+(april "×/⊃(input∘.,input)[1↑{(2020=⍵)/⍳⍴⍵} (a←,input∘.+input)]")
+; is the 1↑ necessary?
+
+;part 2
+(april "a←input")
+(april "trips←,a∘.,,a∘.,a⋄44")
+(april "×/¨trips[(2020=+/¨trips)/⍳⍴trips]")
