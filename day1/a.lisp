ff4d460327895ba825f70d1d7e494590fda8500b
Justin <justin2004@hotmail.com>
2020-12-01 09:24:10 -0500

    note

--- day1/a.lisp
+++ day1/a.lisp
@@ -19,6 +19,7 @@
 ; is the 1↑ necessary?
 
 ;part 2
+; NOTE i had to start sbcl with --dynamic-space-size 4096
 (april "a←input")
 (april "trips←,a∘.,,a∘.,a⋄44")
 (april "×/¨trips[(2020=+/¨trips)/⍳⍴trips]")
