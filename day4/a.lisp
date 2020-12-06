4790efeedfe86722e304f26db19dc489ae75ce49
Justin <justin2004@hotmail.com>
2020-12-06 15:09:04 -0500

    wip

--- day4/a.lisp
+++ day4/a.lisp
@@ -62,6 +62,57 @@
 
 
 
+; part 2
+
+    ; byr (Birth Year) - four digits; at least 1920 and at most 2002.
+(april "byr_valid←{(4=≢⍵) ∧ (1920≤⍎⍵) ∧ 2002≥⍎⍵} ⋄ byr_valid '1920'")
+    ; iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+(april "iyr_valid←{(4=≢⍵) ∧ (2010≤⍎⍵) ∧ 2020≥⍎⍵} ⋄ iyr_valid '2010'")
+    ; eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+(april "eyr_valid←{(4=≢⍵) ∧ (2020≤⍎⍵) ∧ 2030≥⍎⍵} ⋄ eyr_valid '2031'")
+    ; hgt (Height) - a number followed by either cm or in:
+    ;     If cm, the number must be at least 150 and at most 193.
+(april "hgt_valid←{'cm' 'in' ⍳ ⊂(¯2↑⍵)
+                   bounds←('cm' 'in' ⍳ ⊂(¯2↑⍵)) ⊃ (150 193) (59 76)
+                   value←⍎(¯2↓⍵)
+                   (bounds[1]≤value) ∧ value≤bounds[2] }")
+(april "hgt_valid '64cm'")
+
+(april "⊂(¯2↑'151in') 0 ⍳ 'cm' 'in'")
+(april "'cm' 'in' ⍳ ⊂(¯2↑'151in')") ; works
+(april "bounds←('cm' 'in' ⍳ ⊂(¯2↑'64in')) ⊃ (150 193) (59 76)  ")
+(april "value←⍎(¯2↓'64in')")
+(april "(bounds[1]≤value) ∧ value≤bounds[2]")
+    ;     If in, the number must be at least 59 and at most 76.
+    ; hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+(april "hcl_valid←{('#'=⍵[1]) ∧ (7=≢⍵)  ∧ 6=+/(1↓⍵) ∊ '0123456789abcdef'}")
+    ; ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+(april "ecl_valid←{1↑⍵ 0 ∊ 'amb' 'blu' 'brn' 'gry' 'grn' 'hzl' 'oth'} ⋄ ecl_valid 'hzl'")
+    ; pid (Passport ID) - a nine-digit number, including leading zeroes.
+(april "pid_valid←{(9=≢⍵) ∧  ∧/⍵ ∊ '0123456789'} ⋄ pid_valid '123456789a'")
+    ; cid (Country ID) - ignored, missing or not.
+
+
+(april "expected_fields←'byr' 'iyr' 'eyr' 'hgt' 'hcl' 'ecl' 'pid'")
+(april "a←(¯1⌽(' '=input[8])) ⋄ a[1]←1")
+(april "(~∨\\¨':'=a⊂input[8])/¨a⊂input[8]")
+(april "input[8]")
+(april "⍴expected_fields ∩ (~∨\\¨':'=a⊂input[8])/¨a⊂input[8]")
+(april "≢'#6b5442'")
+(april "hcl_valid '#6b5442'")
+(april-f "⍸ 3 3 ⍴ 0 0 1   0 0 0    1 1 0")
+(april "∧/(1↓'#6b5442') ∊ '0123456789abcdef'")
+(april "'a'-'a'")
+(april "'#6b5442'")
+
+(april "⎕IO←0")
+(april "⎕IO")
+
+(april "⎕D")
+(april "⎕A")
+
+(april (with (:state :count-from 0))
+       "⎕IO")
 
 
 
@@ -93,3 +144,11 @@
 (april "'apple' 'pasta'[2]")
 
 (april "input")
+
+(ql:quickload :bordeaux-threads)
+(ql:system-apropos "threads")
+
+(bt:make-thread #'(lambda ()
+                    (april "⍳10")))
+
+
