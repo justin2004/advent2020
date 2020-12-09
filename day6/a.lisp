d95ea0641e0d1fdf35e3b518a827567b6369470e
Justin <justin2004@hotmail.com>
2020-12-08 21:00:48 -0500

    day6 p2
    wip
    found an april bug

--- day6/a.lisp
+++ day6/a.lisp
@@ -2,7 +2,8 @@
 (in-package :april)
 
 ; cat input.txt  | tr "\n" "|" | sed -e 's/||/\n/g' -e 's/|//g' > input1.txt"
-(setf *i* (with-open-file (s "day6/input1.txt" 
+; cat input.txt  | tr "\n" "|" | sed -e 's/||/\n/g' > input2.txt"
+(setf *i* (with-open-file (s "day6/input-sample2.txt" 
                              :direction :input)
             (loop 
               :for line = (read-line s nil)
@@ -18,3 +19,37 @@
 ; part 1
 (april-f "+/+/¨≠¨input")
 ; Mv(f⍤1)Nm           Apply f between vector Mv and each row of Nm
+
+; part 2
+(april-f "≠¨input")
+(april "input")
+; For each group, count the number of questions to which everyone answered "yes"."
+(april "'abc'∩'a|b|c'∩'ab|ac'∩'a|a|a'∩'b|'")
+(april "'ab'∩'ac'")
+(april-f "~'|'=input")
+(april "1 0 1 0 1⊂'a|b|c'")
+(april "∩/¨input")
+(april "('|'=input[3])⊂input[3]")
+(april "1 0 1 0 0 ⊂ ⊃input[3]")
+(april "('|'=input[3])")
+(april "∩/'ab' 'ac'")
+(april "i←'ab|ac'⋄map←'|'=i⋄map[1]←1⋄map⋄ '|'=map⊂i")
+
+(april "i←'ab|ac'⋄map←'|'=i⋄map[1]←1⋄∩/map⊂i") ; works
+(april "i←'ab|ac'⋄map←'|'=i⋄map[1]←1⋄map⊂i") ; works
+(april "i←input ⋄ map←'|'=¨i ⋄ map1←1,¨1↓¨map ⋄ map1⊂¨i") ; works on multiple
+(april "i←input ⋄ map←'|'=¨i ⋄ map1←1,¨1↓¨map ⋄ ∩/(map1⊂¨i)[1]")
+(april "i←input ⋄ map←'|'=¨i ⋄ map1←1,¨1↓¨map ⋄ {∩/⍵}¨map1⊂¨i")
+(april "input")
+(april "apple←3 ⍴ ⊂⍳10 ⋄ (1↑⊃apple[1])←99")
+(april "apple←3 ⍴ ⊂⍳4 ⋄ (1↑⊃apple[1])←99 ⋄ apple")
+(april "apple←3 ⍴ ⊂⍳10 ⋄ apple[1]")
+
+
+(april "aa←(1 0 0) (1 1 0 1 0)⊂¨'abc' 'a|b|c'")
+(april "⍴aa[1]")
+(april "∩/¨aa") ; resume after https://github.com/phantomics/april/issues/94
+(april "{∩/⍵}¨aa")
+
+(april "⊃∩/aa[1]")
+(april "aa[1]")
